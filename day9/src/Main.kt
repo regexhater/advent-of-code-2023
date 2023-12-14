2746f4bd76172fec5218b7e9e64063bd24b3752c
regexhater <przemyslaw.golebski@gmail.com>
2023-12-14 01:21:18 +0100

    Day 9 Part 2

--- day9/src/Main.kt
+++ day9/src/Main.kt
@@ -7,23 +7,13 @@ fun main() {
     val path = Paths.get("src/input.txt")
     val lines = Files.readAllLines(path, StandardCharsets.UTF_8)
     part1(lines)
+    part2(lines)
 }
 
 fun part1(input: MutableList<String>) {
     var sumOfExtrapolatedValues = 0
     for (line in input) {
-        val matrix = mutableListOf<MutableList<Int>>()
-        val ml = mutableListOf<Int>()
-        for (s in line.split(" ")) {
-            ml.add(Integer.parseInt(s))
-        }
-        matrix.add(ml)
-        var diff = getDiffList(ml)
-        matrix.add(diff)
-        while (!diff.all { x -> x == 0 }) {
-            diff = getDiffList(diff)
-            matrix.add(diff)
-        }
+        val matrix = generatePredictionMatrix(line)
         matrix.last().add(0)
         for (i in matrix.size -2 downTo 0) {
             val nextVal = matrix[i].last() + matrix[i + 1].last()
@@ -32,6 +22,40 @@ fun part1(input: MutableList<String>) {
         sumOfExtrapolatedValues += matrix.first().last()
     }
     println("Part 1. The sum of Extrapolated values is equal: $sumOfExtrapolatedValues")
+}
+
+fun part2(input: MutableList<String>) {
+    var sumOfExtrapolatedValues = 0
+    for (line in input) {
+        val matrix = generatePredictionMatrix(line)
+        var newRow = mutableListOf(0)
+        newRow.addAll(matrix.last())
+        matrix[matrix.size - 1] = newRow
+        for (i in matrix.size -2 downTo 0) {
+            val prevVal = matrix[i].first() - matrix[i + 1].first()
+            newRow = mutableListOf(prevVal)
+            newRow.addAll(matrix[i])
+            matrix[i] = newRow
+        }
+        sumOfExtrapolatedValues += matrix.first().first()
+    }
+    println("Part 2. The sum of Prev Extrapolated values is equal: $sumOfExtrapolatedValues")
+}
+
+private fun generatePredictionMatrix(line: String): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    val ml = mutableListOf<Int>()
+    for (s in line.split(" ")) {
+        ml.add(Integer.parseInt(s))
+    }
+    matrix.add(ml)
+    var diff = getDiffList(ml)
+    matrix.add(diff)
+    while (!diff.all { x -> x == 0 }) {
+        diff = getDiffList(diff)
+        matrix.add(diff)
+    }
+    return matrix
 }
 
 fun getDiffList(list: MutableList<Int>) : MutableList<Int> {
